09ca418cc84f042718eb748edb9aa3d122768138
RealDream <157290703+qiaozishun4@users.noreply.github.com>
2025-09-27 12:44:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.getElementById('submit-btn').addEventListener('click', async function (
 
 function getAnswerFromAI(question) {
     // 使用您提供的本地 Ollama 地址和模型名
-    const ipAndPort = '51.15.221.201:11434';
+    const ipAndPort = '46.62.195.29:11434';
     const modelName = 'llama3.2-vision:latest';
     const url = `http://${ipAndPort}/api/generate`;
 
